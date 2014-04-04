41ae034b38812034158c790b2d6b3226db6ceaf4
Chris Zalidis <zalidis@gmail.com>
2014-04-04 17:03:58 +0300

    Add serial interface for motors

--- pandora_motor_hardware_interface/include/pandora_motor_hardware_interface/motor_serial_interface.h
+++ pandora_motor_hardware_interface/include/pandora_motor_hardware_interface/motor_serial_interface.h
@@ -32,109 +32,64 @@
 *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 *  POSSIBILITY OF SUCH DAMAGE.
 *
-* Author: Michael Niarchos
-* Author: Chris Zalidis
+* Author:  Chris Zalidis
 *********************************************************************/
-
-#ifndef XMEGA_SERIAL_INTERFACE
-#define XMEGA_SERIAL_INTERFACE
+#ifndef PANDORA_MOTOR_SERIAL_INTERFACE_MOTOR_HARDWARE_INTERFACE_H
+#define PANDORA_MOTOR_SERIAL_INTERFACE_MOTOR_HARDWARE_INTERFACE_H
 
 #include <stdexcept>
-#include <sys/time.h>
 #include <boost/scoped_ptr.hpp>
 #include <boost/utility.hpp>
+#include <boost/format.hpp>
 
 #include <ros/ros.h>
 #include <serial/serial.h>
 
-#include "pandora_xmega_hardware_interface/enums.h"
-#include "pandora_xmega_hardware_interface/default_sensor.h"
-#include "pandora_xmega_hardware_interface/range_sensor.h"
-#include "pandora_xmega_hardware_interface/battery_sensor.h"
+namespace pandora_hardware_interface {
+  
+  namespace motor {
+  
+    class MotorSerialInterface : private boost::noncopyable
+    {
+     public:
+      MotorSerialInterface(
+          const std::string& device,
+          int speed,
+          int timeout);
+      
+      void init();
+      
+      void read();
+      
+      inline void getSpeeds(int* leftSpeed, int* rightSpeed) const
+      {
+        *leftSpeed = leftSpeed_;
+        *rightSpeed = rightSpeed_;
+      }
+      
+      inline void setSpeeds(int leftSpeed, int rightSpeed)
+      {
+        write(leftSpeed, rightSpeed);
+      }
+      
+     private:
+     
+      void write(int leftSpeed, int rightSpeed);
+      
+     private:
+      
+      int leftSpeed_;
+      int rightSpeed_;
+      
+      const std::string device_;
+      const int speed_;
+      const int timeout_;
+  
+      boost::scoped_ptr<serial::Serial> serialPtr_;
+    };
+  
+  }  // namespace motor 
 
-
-namespace pandora_hardware_interface
-{
-namespace xmega
-{
-
-class SerialIO : private boost::noncopyable
-{
- public:
-  
-  SerialIO(const std::string& device, 
-           int speed, 
-           int timeout);
-  
-  void init();
-  
-  int readMessageType();
-  int readSize(uint16_t *dataSize);
-  int readData(uint16_t dataSize, unsigned char *dataBuffer);
-  int readCRC();
-  bool write(const uint8_t *data, size_t size);
-  
- private:
-  
-  int CRC_;
-  
-  const std::string device_;
-  const int speed_;
-  const int timeout_;
-  
-  boost::scoped_ptr<serial::Serial> serialPtr_;
-
-};
-
-class XmegaSerialInterface : private boost::noncopyable
-{
- public:
-
-  XmegaSerialInterface(const std::string& device, 
-                       int speed, 
-                       int timeout);
-  void init();
-  
-  void read();
-
-  inline void getBatteryData(double* psuVoltage, double* motorVoltage) const
-  {
-    *psuVoltage = batterySensor_.psuVoltage;
-    *motorVoltage = batterySensor_.motorVoltage;
-  }
-  
-  inline RangeMap getRangeData() const 
-  {
-    return rangeSensors_.sensors;
-  }
-  
- private:
- 
-  void receiveData();
-  int processData();
-  
-  SensorBase* getSensor(int sensorType);
-  
- private:
-  
-  unsigned char *pdataBuffer_;
-  
-  int currentState_;
-  timeval tim_;
-  double t1_;
-  double t2_;
-  uint16_t dataSize_;
-  
-  DefaultSensor defaultSensor_;
-  BatterySensor batterySensor_;
-  RangeSensor rangeSensors_;
-    
-  SerialIO serialIO_;
-};
-
-static unsigned char myatoi(char *array, int size);
-
-}  // namespace xmega
 }  // namespace pandora_hardware_interface
 
-#endif
+#endif  // PANDORA_MOTOR_HARDWARE_INTERFACE_MOTOR_HARDWARE_INTERFACE_H
