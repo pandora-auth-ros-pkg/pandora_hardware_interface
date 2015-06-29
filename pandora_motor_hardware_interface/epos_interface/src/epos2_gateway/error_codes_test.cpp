f5e6fe53e66d4cc8822c8244733e8559e56ef6c1
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-06-29 04:23:31 +0300

    Loading error codes from file support --- Initial commit

---
+++ pandora_motor_hardware_interface/epos_interface/src/epos2_gateway/error_codes_test.cpp
@@ -0,0 +1,95 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2015, P.A.N.D.O.R.A. Team.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the P.A.N.D.O.R.A. Team nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author:     Konstantinos Panayiotou   <klpanagi@gmail.com>
+* Maintainer: Konstantinos Panayiotou   <klpanagi@gmail.com>
+*********************************************************************/
+
+#include "epos2_gateway/error_codes.h"
+
+#include <stdint.h>
+#include <map>
+#include <cstring>
+#include <iostream>  // Input
+#include <fstream>  // Input
+#include <sstream>
+#include <stdlib.h>
+
+bool hasSubstr(std::string base_str, std::string token_str)
+{
+  if(base_str.find(token_str) != std::string::npos)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int main(int argc, char** argv)
+{
+  //std::map<int, std::string> map_;
+  //std::cout << "Hello\n";
+  //std::ifstream file;  
+  //std::string token;
+  //file.open("/home/klpanagi/pandora_ws/src/pandora_hardware_interface/pandora_motor_hardware_interface/epos_interface/src/epos2_gateway/error_codes");
+  //std::string line;
+  //int error_code;
+  //std::string error_str;
+  //std::string error_code_str;
+  //if(file.is_open())
+  //{
+    //while(std::getline(file, line))
+    //{
+      //std::istringstream iss(line);
+      //getline(iss, error_code_str, ',');
+
+      //std::istringstream hex_str(error_code_str);
+      //hex_str >> std::hex >> error_code;
+
+      //getline(iss, error_str, ',');
+      //if(error_str.at(0) == ' ')
+      //{
+        //error_str.erase(0, 1);
+      //}
+      //std::cout << "ErrorCode_int: " << error_code << ", ErrorCode_str: " 
+        //<< error_code_str << ", Error: " << error_str << "\n";
+    //}
+  //}
+  pandora_hardware_interface::motor::ErrorCodes err;
+  std::string error_codes_file = "/home/klpanagi/pandora_ws/src/pandora_hardware_interface/pandora_motor_hardware_interface/epos_interface/src/epos2_gateway/error_codes";
+  err.fillErrorCodesMap(error_codes_file);
+return 1 ;
+}
